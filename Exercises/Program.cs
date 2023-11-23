374493d5ce45da054344637b0f8ad3a4a37064dc
Ata Sahin <atasahin05@gmail.com>
2023-11-23 14:41:32 +0300

    constructors

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,32 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ClassTutorial
+namespace ClassTutiorial
 {
-    public class Musteri
-    {
-        public int ID;
-        public string Adi;
-        public string Soyadi;
-        public void info()
-        {
-            Console.WriteLine("Müşteri ID: {0} İsim: {1} {2}", ID, Adi, Soyadi);
-        }
-
-    }
     class Program
     {
-        static void Main(string[] args)
+        public class Musteri
         {
-            Musteri m1 = new Musteri();
-            m1.ID = 1;
-            m1.Adi = "Ahmet";
-            m1.Soyadi = "Yılmaz";
-            m1.info();
-            Musteri m2 = new Musteri();
-            m2.ID = 2;
-            m2.Adi = "Mehmet";
-            m2.Soyadi = "Yılmaz";
-            m2.info();
+            public int ID;
+            public string Adi;
+
+            public Musteri(int ID, string Adi)
+            {
+                this.ID = ID;
+                this.Adi = Adi;
+                Console.WriteLine("Musteri olusturuldu");
+            }
+            public Musteri()
+            {
+                Console.WriteLine("Musteri methodu 2");
+            }
+            public Musteri(int ID)
+            {
+                Console.WriteLine("Musteri methodu 3");
+            }
+            public void PrintName()
+            {
+                Console.WriteLine("ID: {0} Adi: {1}", ID, Adi);
+            }
+        }
+
+        static void Main()
+        {
+
+            Musteri m = new Musteri();
+
+            m.ID = 1;
+            m.Adi = "Ata";
+            m.PrintName();
+            Musteri m2 = new Musteri(2);
+            m2.Adi = "Sahin";
+            m2.PrintName();
+            Musteri m3 = new Musteri(3, "Adil");
+            m3.PrintName();
         }
     }
 }
