33dc8070f4422e6b3c109f2858a1f8e5ef21ede0
Ata Sahin <atasahin05@gmail.com>
2023-11-24 11:02:28 +0300

    test

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,51 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ClassTutiorial
+public class Circle
 {
-    class Program
+
+    public double Radius { get; set; }
+
+
+    public static double CalculateArea(double radius)
     {
-        public class Musteri
-        {
-            public int ID;
-            public string Adi;
+        return Math.PI * Math.Pow(radius, 2);
+    }
+}
 
-            public Musteri(int ID, string Adi)
-            {
-                this.ID = ID;
-                this.Adi = Adi;
-                Console.WriteLine("Musteri olusturuldu");
-            }
-            public Musteri()
-            {
-                Console.WriteLine("Musteri methodu 2");
-            }
-            public Musteri(int ID)
-            {
-                Console.WriteLine("Musteri methodu 3");
-            }
-            public void PrintName()
-            {
-                Console.WriteLine("ID: {0} Adi: {1}", ID, Adi);
-            }
-        }
+class Program
+{
+    static void Main()
+    {
 
-        static void Main()
-        {
+        Console.Write("Dairenin yarıçapını girin: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
 
-            Musteri m = new Musteri();
 
-            m.ID = 1;
-            m.Adi = "Ata";
-            m.PrintName();
-            Musteri m2 = new Musteri(2);
-            m2.Adi = "Sahin";
-            m2.PrintName();
-            Musteri m3 = new Musteri(3, "Adil");
-            m3.PrintName();
-        }
+        double area = Circle.CalculateArea(radius);
+
+
+        Console.WriteLine("Dairenin Alanı: " + area);
     }
 }
